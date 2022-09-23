2c444d3759c5f0ee2f115176bdeb126ad8fa08db
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-09-23 16:44:58 -0300

    Update Ej6.cpp

--- Ej6/Ej6.cpp
+++ Ej6/Ej6.cpp
@@ -1,3 +1,5 @@
+/*Desarrollar una función que dada una cola de personas (de cada persona se tiene dni y nombre) y un dni, si esta persona está en la cola, la mueva al último lugar de la misma.*/ 
+
 #include <iostream>
 #include <string.h>
 
