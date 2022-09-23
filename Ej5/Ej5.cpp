1a15a0f6972100eaa3a601f7f57bd264afb404a1
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-09-23 16:44:19 -0300

    Update Ej5.cpp

--- Ej5/Ej5.cpp
+++ Ej5/Ej5.cpp
@@ -1,3 +1,5 @@
+/*Desarrollar una función que dada una cola elimine 4 nodos de la misma, y devuelva un valor true si ello fue posible, de lo contrario un valor falso.*/
+
 #include <iostream>
 
 using namespace std;
